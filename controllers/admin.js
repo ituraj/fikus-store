5c10623d4185d2d012df1117b152f4a9c05c6ab5
Izabela Turaj <i.turaj@gmail.com>
2020-02-02 01:27:30 +0100

    navigation setup

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,14 +17,8 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      products: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
-    });
+  res.render('admin/products', {
+    pageTitle: 'Admin Products',
+    path: '/admin/products',
   });
 };
